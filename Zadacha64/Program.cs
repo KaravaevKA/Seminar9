bf86cd3d8e1c23482886fd524af949da0544b315
Kirill Karavaev <coffee.2016@yandex.ru>
2023-02-04 10:22:23 +0300

    64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+// void NumReturn(int N, int m)
+// {
+//     if (m <= N)
+//     {
+//         NumReturn(m+1, N);
+//         Console.WriteLine($"{m}");
+//     }
+// }
+// Console.WriteLine("Введите число N");
+// int NumN = Convert.ToInt32(Console.ReadLine());
+// int NumM = 1;
+// NumReturn(NumM, NumN);
+
+void OutputNumbers(int m, int n) 
+    {
+      if (m <= n) {
+          OutputNumbers(m + 1, n);
+          Console.Write($"{m} ");
+      }
+    }
+
+    Console.Write("Введите число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int m = 1;
+    OutputNumbers(m, n);
